f47cb29fc8a4828f18698883e9cf998978d08c15
hema*g1thub <hemasunny@gmail.com>
2024-08-12 20:59:59 +0530

    initial

---
+++ first.go
@@ -0,0 +1,9 @@
+package first
+
+import "fmt"
+import "math"
+
+func Greet() {
+	fmt.Printf("Good Morning")
+	fmt.Printf("value of e = %e",math.E)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hemasunny/gosamples
+
+go 1.20
