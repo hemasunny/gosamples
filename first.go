e1786de10c7b30c335f4b219f6d7d01545af05b8
hema*g1thub <hemasunny@gmail.com>
2024-08-12 21:08:46 +0530

    second main

--- first.go
+++ first.go
@@ -7,3 +7,8 @@ func Greet() {
 	fmt.Printf("Good Morning")
 	fmt.Printf("value of e = %e",math.E)
 }
+
+func Evening() {
+	fmt.Printf("Good Evening")
+	fmt.Printf("value of e = %e",math.Sqrt(25.7))
+}
