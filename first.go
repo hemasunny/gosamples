ef6bea5fdd1439eb9198203fc4598190bb5c375b
hema*g1thub <hemasunny@gmail.com>
2024-08-12 21:17:04 +0530

    third

--- first.go
+++ first.go
@@ -1,14 +1,21 @@
 package first
 
-import "fmt"
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func Greet() {
 	fmt.Printf("Good Morning")
-	fmt.Printf("value of e = %e",math.E)
+	fmt.Printf("value of e = %e", math.E)
 }
 
 func Evening() {
 	fmt.Printf("Good Evening")
-	fmt.Printf("value of e = %e",math.Sqrt(25.7))
+	fmt.Printf("value of e = %e", math.Sqrt(25.7))
+}
+
+func Day() {
+	fmt.Printf("Good Day")
+	fmt.Printf("value of e = %e", math.Sqrt(25))
 }
